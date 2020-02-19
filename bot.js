d94e1c3fb138202c14290299b6a70e71b27d1c20
Matheus Breder <math.upa@hotmail.com>
2020-02-18 23:05:54 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,25 +1,14 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
 
- 
-
 client.on('ready', () => {
-
-    console.log('I am ready!');
-
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
- 
-
-client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
 
  
